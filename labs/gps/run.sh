acf41fbfc754bb0329c113c732668e0761e77df2
fcebron <fcebronATfree.fr>
2017-11-20 17:56:14 +0100

    End exo1, begining exo2

--- labs/gps/run.sh
+++ labs/gps/run.sh
@@ -4,5 +4,5 @@ SCRIPT=`readlink -f $0`
 ROOT_DIR=`dirname $SCRIPT`
 
 export LD_LIBRARY_PATH=$ROOT_DIR/lib
-#LD_PRELOAD=$ROOT_DIR/lib/libhook.so $ROOT_DIR/bin/gps
+LD_PRELOAD=$ROOT_DIR/lib/libhook.so $ROOT_DIR/bin/gps
 $ROOT_DIR/bin/gps
