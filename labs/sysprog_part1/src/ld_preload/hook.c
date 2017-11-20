0b9e7c4578c085d762abd3618c536dcef061fd88
fcebron <fcebronATfree.fr>
2017-11-20 17:56:14 +0100

    End exo1, begining exo2

--- labs/sysprog_part1/src/ld_preload/hook.c
+++ labs/sysprog_part1/src/ld_preload/hook.c
@@ -5,6 +5,11 @@
 #include <time.h>
 #include <stdlib.h>
 
+// For question 12:
+#include <signal.h>
+#include <errno.h>
+#include <ptmx.h>
+
 //#include "nmea.h"
 #define KNOT_TO_KMH 1.852
 
@@ -27,4 +32,31 @@ int knot_to_kmh_str(float not, size_t size, char * format, char * kmh_str)
 }
 
 // Question 12 : write printf with a signal handler
+// struct PTMX ptmx;
 
+// void signals_handler(int signal_number)
+// {
+// 	/**
+// 	 * Careful, it is awful!
+// 	 */
+// 	puts("Hey There!\n");
+//     //printf("Signal catched.\n");
+//     ptmxclose(&ptmx);
+//     //exit(EXIT_SUCCESS);
+// }
+
+
+// int printf(const char *format, ...){
+// 	// Used to avoid the program to stop with SIGINT
+	 
+
+// 	// signals handler
+// 	struct sigaction action;
+// 	action.sa_handler = signals_handler;
+// 	sigemptyset(& (action.sa_mask));
+// 	action.sa_flags = 0;
+// 	sigaction(SIGINT, & action, NULL);
+// 	//puts(format);
+// 	puts("Warning! This may have no effect!\n");
+// 	return EXIT_SUCCESS;
+// }
