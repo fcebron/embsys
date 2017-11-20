57a889ef9eaff298022cee2ad00dfc18bb193764
fcebron <fcebronATfree.fr>
2017-11-20 15:09:28 +0100

    starting TP1

--- labs/gps/debug.sh
+++ labs/gps/debug.sh
@@ -4,5 +4,4 @@ SCRIPT=`readlink -f $0`
 ROOT_DIR=`dirname $SCRIPT`
 
 export LD_LIBRARY_PATH=$ROOT_DIR/lib
-#LD_PRELOAD=$ROOT_DIR/lib/libhook.so $ROOT_DIR/bin/gps
-$ROOT_DIR/bin/gps
+gdb $ROOT_DIR/bin/gps $ROOT_DIR/vgcore.7938
--- labs/sysprog_part1/src/ld_preload/hook.c
+++ labs/sysprog_part1/src/ld_preload/hook.c
@@ -1,4 +1,30 @@
 // Question 8 : write here the buggy function without errors
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+//#include "nmea.h"
+#define KNOT_TO_KMH 1.852
+
+int iteration = 0;
+
+int knot_to_kmh_str(float not, size_t size, char * format, char * kmh_str)
+{
+    float kmh = KNOT_TO_KMH * not;
+    snprintf(kmh_str, size, format, kmh);
+
+#ifndef GPS_OK
+    iteration++;
+    if (iteration == 2)
+    {
+        puts("No Signal");
+    }
+#endif
+
+    return kmh;
+}
 
 // Question 12 : write printf with a signal handler
 
