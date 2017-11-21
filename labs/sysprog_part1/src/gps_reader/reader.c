5818b9f37c93da3261eb2aa2824636dc9d597ddf
fcebron <fcebronATfree.fr>
2017-11-21 16:46:46 +0100

    Question 18

--- labs/sysprog_part1/src/gps_reader/reader.c
+++ labs/sysprog_part1/src/gps_reader/reader.c
@@ -11,21 +11,26 @@
 //-----------------------------------------------------------------------------
 int main(int argc, char *argv [])
 {
-    char * port = NULL;
+    char * port = NULL,
+         * port2 = NULL;
 
     // parse comand line
-    if (argc != 3)
+    if ((argc != 3) && (argc != 5))
     {
-        fprintf(stderr, "Invalid usage: reader -p port_name\n");
+        fprintf(stderr, "Invalid usage: gps_reader -p port_name\n or : gps_reader -d port_name1 port_name2\n");
         exit(EXIT_FAILURE);
     }
 
-    char * options = "p:";
+    char * options = "p:d:";
     int option;
     while((option = getopt(argc, argv, options)) != -1)
     {
         switch(option)
-        {
+        { 
+            case 'd':
+                port2 = optarg;
+                break;
+
             case 'p':
                 port = optarg;
                 break;
@@ -36,17 +41,28 @@ int main(int argc, char *argv [])
         }
     }
 
-    // open serial port
+    // Open serial port
+    //  Port 1:
     int fd = open(port, O_RDWR | O_NOCTTY);
     if (fd == -1)
     {
-        perror("open");
+        perror("open - port1");
         exit(EXIT_FAILURE);
     }
     tcflush(fd, TCIOFLUSH);
 
+    //  Port 2:
+    int fd2 = open(port2, O_RDWR | O_NOCTTY);
+    if (fd2 == -1)
+    {
+        perror("open - port2");
+        exit(EXIT_FAILURE);
+    }
+    tcflush(fd2, TCIOFLUSH);
+
     // read port
-    char buff[50];
+    char buff[50],
+         buff2[50];
     fd_set fdset;
 
     while(1)
@@ -55,16 +71,30 @@ int main(int argc, char *argv [])
 
         FD_ZERO(&fdset);
         FD_SET(fd, &fdset);
+        FD_SET(fd2, &fdset);
 
-        select(fd+1, &fdset, NULL, NULL, NULL);
-
+        select(fd+2, &fdset, NULL, NULL, NULL);
+        
+        //  Port 1:
         if (FD_ISSET(fd, &fdset))
         {
             int bytes = read (fd, buff, sizeof(buff));
 
             if (bytes > 0)
             {
-                printf("%s\n", buff);
+                printf("Port1: %s\n", buff);
+                fflush(stdout);
+            }
+        }
+
+        //  Port 2:
+        if (FD_ISSET(fd2, &fdset))
+        {
+            int bytes = read (fd2, buff2, sizeof(buff2));
+
+            if (bytes > 0)
+            {
+                printf("Port2: %s\n", buff2);
                 fflush(stdout);
             }
         }
